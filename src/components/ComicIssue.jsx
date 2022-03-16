11de9b5563b1897f8fdfe5304ececda6db41d48e
nicolasscarafia <nicolasscarafia@outlook.com>
2022-03-16 14:49:03 -0300

    Modificaciones para ComicIssue

--- src/components/ComicIssue.jsx
+++ src/components/ComicIssue.jsx
@@ -3,26 +3,10 @@ import ItemCount from './ItemCount'
 
 const ComicIssue = ({ comic }) => {
 
-    const [compra, setCompra] = useState(0)
-
-    const aumentarCompra = () => {
-        if (comic.stock > compra) {
-            setCompra(compra + 1)
-        } else {
-            console.log
-                (`Ya alcanzaste el máximo de unidades disponibles para ${comic.nombre}`)
-        }
-    }
-
-    const disminuirCompra = () => {
-        if (!compra == 0) {
-            setCompra(compra - 1)
-        }
-    }
-
     return (
 
         <div className='m-4'>
+
             <div className='comic-cover'>
                 <img src={comic.cover} alt={`Cover de ${comic.nombre}`} />
             </div>
@@ -37,6 +21,7 @@ const ComicIssue = ({ comic }) => {
                     {comic.stock > 0 ? 'Comprar' : 'Sin stock'}
                 </button>
             </div>
+            
         </div>
 
     )
